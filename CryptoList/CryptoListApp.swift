677d51a97e7fe7539d70a5a3dc48a1caba7b9743
OBoianovskyi <oboianovskiy@gmail.com>
2024-11-07 19:48:26 +0200

    initial commit

---
+++ CryptoList/CryptoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoListApp.swift
+//  CryptoList
+//
+//  Created by Oleksandr Boianovskyi on 07.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
